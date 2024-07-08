a14ae0494ac039196c18a58f6a2bb7db3e590299
kentoespdam <kentoes.pdam@gmail.com>
2024-07-08 15:38:22 +0700

    add kota and gpa at pendidikan put request
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/dto/profil/pendidikan/PendidikanPostRequest.java
+++ src/main/java/id/perumdamts/kepegawaian/dto/profil/pendidikan/PendidikanPostRequest.java
@@ -56,11 +56,4 @@ public class PendidikanPostRequest {
         entity.setTanggalPengajuan(LocalDateTime.now());
         return entity;
     }
-
-//    public static Pendidikan from(Biodata biodata, JenjangPendidikan jenjangPendidikan) {
-//        Pendidikan entity = new Pendidikan();
-//        entity.setBiodata(biodata);
-//        entity.setJenjangPendidikan(jenjangPendidikan);
-//        return entity;
-//    }
 }
