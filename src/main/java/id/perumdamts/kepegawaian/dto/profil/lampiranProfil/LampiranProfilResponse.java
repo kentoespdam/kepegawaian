ba6793033a98b94eebc241df8af23fe66119daa0
kentoespdam <kentoes.pdam@gmail.com>
2024-07-10 14:12:09 +0700

    add disetujui lampiran pendidikan
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/dto/profil/lampiranProfil/LampiranProfilResponse.java
+++ src/main/java/id/perumdamts/kepegawaian/dto/profil/lampiranProfil/LampiranProfilResponse.java
@@ -1,8 +1,12 @@
 package id.perumdamts.kepegawaian.dto.profil.lampiranProfil;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import id.perumdamts.kepegawaian.entities.commons.EJenisLampiranProfil;
 import id.perumdamts.kepegawaian.entities.profil.LampiranProfil;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class LampiranProfilResponse {
@@ -11,6 +15,10 @@ public class LampiranProfilResponse {
     private Long refId;
     private String fileName;
     private String notes;
+    private boolean disetujui;
+    private String disetujuiOleh;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime tanggalDisetujui;
 
     public static LampiranProfilResponse from(LampiranProfil entity) {
         LampiranProfilResponse response = new LampiranProfilResponse();
@@ -19,6 +27,9 @@ public class LampiranProfilResponse {
         response.setRefId(entity.getRefId());
         response.setFileName(entity.getFileName());
         response.setNotes(entity.getNotes());
+        response.setDisetujui(entity.getDisetujui());
+        response.setDisetujuiOleh(entity.getDisetujuiOleh());
+        response.setTanggalDisetujui(entity.getTanggalDisetujui());
         return response;
     }
 }
