58180d7b5c02c142de0b89d15787e980783938bd
kentoespdam <kentoes.pdam@gmail.com>
2024-07-11 15:43:35 +0700

    get file lampiran profile
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/controllers/profil/LampiranProfilController.java
+++ src/main/java/id/perumdamts/kepegawaian/controllers/profil/LampiranProfilController.java
@@ -4,16 +4,14 @@ import id.perumdamts.kepegawaian.dto.appwrite.AppwriteUser;
 import id.perumdamts.kepegawaian.dto.commons.CustomResult;
 import id.perumdamts.kepegawaian.dto.commons.ErrorResult;
 import id.perumdamts.kepegawaian.dto.profil.lampiranProfil.LampiranProfilAcceptRequest;
+import id.perumdamts.kepegawaian.entities.commons.EJenisLampiranProfil;
 import id.perumdamts.kepegawaian.services.profil.lampiranProfil.LampiranProfilService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,11 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class LampiranProfilController {
     private final LampiranProfilService service;
 
+    @GetMapping("/file/{jenis}/{id}")
+    public ResponseEntity<?> getFile(@PathVariable EJenisLampiranProfil jenis, @PathVariable Long id) {
+        return service.getFileLampiranById(jenis, id);
+    }
+
     @PostMapping("/accept")
     public ResponseEntity<?> acceptLampiran(@Valid @RequestBody LampiranProfilAcceptRequest request, Errors errors) {
         if (errors.hasErrors()) return ErrorResult.build(errors);
         AppwriteUser appwriteUser = (AppwriteUser) SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal();
         return CustomResult.save(service.acceptLampiran(request, appwriteUser.getName()));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return CustomResult.delete(service.deleteById(id));
     }
 }
