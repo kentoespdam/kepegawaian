137ebe4df9a6a3df51be85ae7c54e9fb94b07f7e
kentoespdam <kentoes.pdam@gmail.com>
2024-08-08 10:26:16 +0700

    change jenisSk to Enum
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/repositories/master/JenisSkRepository.java
+++
@@ -1,8 +0,0 @@
-package id.perumdamts.kepegawaian.repositories.master;
-
-import id.perumdamts.kepegawaian.entities.master.JenisSk;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface JenisSkRepository extends JpaRepository<JenisSk, Long>, JpaSpecificationExecutor<JenisSk> {
-}
--- src/main/java/id/perumdamts/kepegawaian/repositories/master/JenisSpRepository.java
+++
@@ -1,8 +0,0 @@
-package id.perumdamts.kepegawaian.repositories.master;
-
-import id.perumdamts.kepegawaian.entities.master.JenisSp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface JenisSpRepository extends JpaRepository<JenisSp, Long>, JpaSpecificationExecutor<JenisSp> {
-}
