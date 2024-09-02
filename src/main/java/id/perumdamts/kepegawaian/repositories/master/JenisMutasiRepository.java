35c9171fd6f2d2f3b749ae5261defd9da7839fee
kentoespdam <kentoes.pdam@gmail.com>
2024-09-02 16:03:22 +0700

    remove master jenis mutasi
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/repositories/master/JenisMutasiRepository.java
+++
@@ -1,8 +0,0 @@
-package id.perumdamts.kepegawaian.repositories.master;
-
-import id.perumdamts.kepegawaian.entities.master.JenisMutasi;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface JenisMutasiRepository extends JpaRepository<JenisMutasi, Long>, JpaSpecificationExecutor<JenisMutasi> {
-}
