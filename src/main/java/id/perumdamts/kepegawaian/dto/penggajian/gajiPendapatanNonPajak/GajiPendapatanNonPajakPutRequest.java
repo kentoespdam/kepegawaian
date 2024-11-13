ec044224d491981c7de39457256bd2b724adb7aa
kentoespdam <kentoes.pdam@gmail.com>
2024-11-13 13:51:26 +0700

    Pendapatan Non Pajak CRUD done
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

---
+++ src/main/java/id/perumdamts/kepegawaian/dto/penggajian/gajiPendapatanNonPajak/GajiPendapatanNonPajakPutRequest.java
@@ -0,0 +1,11 @@
+package id.perumdamts.kepegawaian.dto.penggajian.gajiPendapatanNonPajak;
+
+import id.perumdamts.kepegawaian.entities.penggajian.GajiPendapatanNonPajak;
+
+public class GajiPendapatanNonPajakPutRequest extends GajiPendapatanNonPajakPostRequest {
+    public static GajiPendapatanNonPajak toEntity(GajiPendapatanNonPajak entity, GajiPendapatanNonPajakPutRequest request) {
+        entity.setKode(request.getKode());
+        entity.setNominal(request.getNominal());
+        return entity;
+    }
+}
