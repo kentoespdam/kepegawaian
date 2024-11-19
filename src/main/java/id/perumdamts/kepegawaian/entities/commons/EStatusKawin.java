522f787468a7299322973ce90ee9238617686a61
kentoespdam <kentoes.pdam@gmail.com>
2024-11-19 14:48:06 +0700

    change enum status kawin
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/entities/commons/EStatusKawin.java
+++ src/main/java/id/perumdamts/kepegawaian/entities/commons/EStatusKawin.java
@@ -3,8 +3,9 @@ package id.perumdamts.kepegawaian.entities.commons;
 public enum EStatusKawin {
     BELUM_KAWIN("Belum Kawin"),
     KAWIN("Kawin"),
-    CERAI_HIDUP("Cerai Hidup"),
-    CERAI_MATI("Cerai Mati");
+    JANDA_DUDA("Janda / Duda"),
+    MENIKAH_SEKANTOR("Menikah Sekantor");
+    TIDAK_TAHU("Tidak Tahu");
 
     private final String label;
 
