48a10d3fb6f836be46d73325c1005d545a2a1292
kentoespdam <kentoes.pdam@gmail.com>
2025-01-15 08:50:17 +0700

    add kafka
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

---
+++ src/test/java/id/perumdamts/kepegawaian/kafka/JajalKafkaTest.java
@@ -0,0 +1,17 @@
+package id.perumdamts.kepegawaian.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootTest
+public class JajalKafkaTest {
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    @Test
+    public void test() {
+        template.send("kepegawaian", "test");
+    }
+}
