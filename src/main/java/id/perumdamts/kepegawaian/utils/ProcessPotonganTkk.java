e75661b0b96bf3bd5ccbc4d56e7dde01339d1f0e
kentoespdam <kentoes.pdam@gmail.com>
2025-02-24 15:11:40 +0700

    change proses read data potongan tkk
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/utils/ProcessPotonganTkk.java
+++ src/main/java/id/perumdamts/kepegawaian/utils/ProcessPotonganTkk.java
@@ -1,7 +1,5 @@
 package id.perumdamts.kepegawaian.utils;
 
-import id.perumdamts.kepegawaian.entities.penggajian.GajiBatchRoot;
-
 public interface ProcessPotonganTkk {
-    void process(GajiBatchRoot params);
+    void process(String rootBatchId);
 }
