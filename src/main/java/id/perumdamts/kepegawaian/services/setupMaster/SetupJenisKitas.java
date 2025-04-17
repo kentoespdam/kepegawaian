32daf185df227fd2c4383358a7b04e1466811372
kentoespdam <kentoes.pdam@gmail.com>
2025-04-17 07:59:54 +0700

    change data setup kitas
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/services/setupMaster/SetupJenisKitas.java
+++ src/main/java/id/perumdamts/kepegawaian/services/setupMaster/SetupJenisKitas.java
@@ -1,26 +1,34 @@
 package id.perumdamts.kepegawaian.services.setupMaster;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import id.perumdamts.kepegawaian.entities.master.JenisKitas;
 import id.perumdamts.kepegawaian.repositories.master.JenisKitasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SetupJenisKitas implements SetupMaster {
     @Autowired
-    private JenisKitasRepository jenisKitasRepository;
+    private JenisKitasRepository repository;
 
     @Override
     public void insertBatch() throws JsonProcessingException {
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        String jsonData = "[{\"nama\":\"KTP\"},{\"nama\":\"BPJS\"},{\"nama\":\"KK\"},{\"nama\":\"NPWP\"},{\"nama\":\"SIM\"},{\"nama\":\"Dapenma\"},{\"nama\":\"JPn\"},{\"nama\":\"Yakan\"},{\"nama\":\"Korpri\"},{\"nama\":\"ID Card\"}]";
-        List<JenisKitas> jenisKitasList = mapper.readValue(jsonData, new TypeReference<>() {
-        });
-        jenisKitasRepository.saveAll(jenisKitasList);
+        List<JenisKitas> list = new ArrayList<>();
+        list.add(new JenisKitas(1L, "KTP"));
+        list.add(new JenisKitas(1L, "NPWP"));
+        list.add(new JenisKitas(2L, "Jamsostek"));
+        list.add(new JenisKitas(3L, "ASKES"));
+        list.add(new JenisKitas(4L, "KTP"));
+        list.add(new JenisKitas(5L, "SIM"));
+        list.add(new JenisKitas(6L, "Dapenma"));
+        list.add(new JenisKitas(7L, "JPn"));
+        list.add(new JenisKitas(8L, "Yakan"));
+        list.add(new JenisKitas(9L, "Inkop Pamsi"));
+        list.add(new JenisKitas(10L, "Korpri"));
+        list.add(new JenisKitas(11L, "ID Card"));
+        repository.saveAll(list);
     }
 }
