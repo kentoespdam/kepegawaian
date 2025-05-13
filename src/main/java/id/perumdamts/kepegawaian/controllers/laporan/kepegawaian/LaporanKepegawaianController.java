27c67b6186177405c62f90726e858f2bf698ec5c
kentoespdam <kentoes.pdam@gmail.com>
2025-05-13 12:59:05 +0700

    add query
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/controllers/laporan/kepegawaian/LaporanKepegawaianController.java
+++ src/main/java/id/perumdamts/kepegawaian/controllers/laporan/kepegawaian/LaporanKepegawaianController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -51,8 +52,8 @@ public class LaporanKepegawaianController {
     }
 
     @GetMapping("/so")
-    public ResponseEntity<?> lapSO() {
-        return service.getHtml("/so/template");
+    public ResponseEntity<?> lapSO(@RequestParam String script_url) {
+        return service.getHtml("/so/template?script_url=" + script_url);
     }
 
     @GetMapping("/statistik/golongan")
