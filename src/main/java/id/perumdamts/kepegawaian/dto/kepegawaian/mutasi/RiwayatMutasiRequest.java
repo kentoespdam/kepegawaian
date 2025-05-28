8a49141d6034006723f3c733532358de28e3f7a6
kentoespdam <kentoes.pdam@gmail.com>
2025-05-28 09:59:21 +0700

    add filter jenis mutasi
    Signed-off-by: kentoespdam <kentoes.pdam@gmail.com>

--- src/main/java/id/perumdamts/kepegawaian/dto/kepegawaian/mutasi/RiwayatMutasiRequest.java
+++ src/main/java/id/perumdamts/kepegawaian/dto/kepegawaian/mutasi/RiwayatMutasiRequest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import id.perumdamts.kepegawaian.dto.commons.CommonPageRequest;
 import id.perumdamts.kepegawaian.entities.commons.EJenisMutasi;
 import id.perumdamts.kepegawaian.entities.kepegawaian.RiwayatMutasi;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.jpa.domain.Specification;
@@ -18,7 +16,6 @@ public class RiwayatMutasiRequest extends CommonPageRequest {
     private Long pegawaiId;
     private Long riwayatSkId;
     private String nomorSk;
-    @Enumerated(EnumType.ORDINAL)
     private EJenisMutasi jenisMutasi;
     private Long organisasiId;
     private String namaOrganisasi;
@@ -35,6 +32,8 @@ public class RiwayatMutasiRequest extends CommonPageRequest {
                 (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("pegawai").get("id"), pegawaiId);
         Specification<RiwayatMutasi> riwayatSkSpec = Objects.isNull(riwayatSkId) ? null :
                 (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("riwayatSk").get("id"), riwayatSkId);
+        Specification<RiwayatMutasi> jenisMutasiSpec = Objects.isNull(jenisMutasi) ? null :
+                (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("jenisMutasi"), jenisMutasi);
         Specification<RiwayatMutasi> skSpec = Objects.isNull(nomorSk) ? null :
                 (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("riwayatSk").get("nomorSk"), nomorSk);
         Specification<RiwayatMutasi> organisasiSpec = Objects.isNull(organisasiId) ? null :
@@ -53,7 +52,7 @@ public class RiwayatMutasiRequest extends CommonPageRequest {
                 (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("jabatanLama").get("id"), jabatanLamaId);
         Specification<RiwayatMutasi> namaJabatanLamaSpec = Objects.isNull(namaJabatanLama) ? null :
                 (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("jabatanLama").get("nama"), namaJabatanLama);
-        return Specification.where(pegawaiSpec).and(riwayatSkSpec).and(skSpec).and(organisasiSpec).and(namaOrganisasiSpec)
+        return Specification.where(pegawaiSpec).and(riwayatSkSpec).and(jenisMutasiSpec).and(skSpec).and(organisasiSpec).and(namaOrganisasiSpec)
                 .and(jabatanSpec).and(namaJabatanSpec).and(organisasiLamaSpec).and(namaOrganisasiLamaSpec)
                 .and(jabatanLamaSpec).and(namaJabatanLamaSpec);
     }
