7de3f9ebb8ab97822c26c0bbe46d67ed23ce68f6
Kentoes PDAM <kentoes.pdam@gmail.com>
2025-10-17 14:32:38 +0700

    bug fix request Pengajuan Cuti

--- src/main/java/id/perumdamts/kepegawaian/dto/cuti/pengajuan/CutiPengajuanRequest.java
+++ src/main/java/id/perumdamts/kepegawaian/dto/cuti/pengajuan/CutiPengajuanRequest.java
@@ -6,9 +6,15 @@ import id.perumdamts.kepegawaian.entities.commons.EApprovalCutiStatus;
 import id.perumdamts.kepegawaian.entities.commons.EJenisPengajuanCuti;
 import id.perumdamts.kepegawaian.entities.cuti.CutiPegawai;
 import id.perumdamts.kepegawaian.utils.SpecificationBuilder;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -30,11 +36,22 @@ public class CutiPengajuanRequest extends CommonPageRequest {
                 .addEqual(pegawaiId, "pegawai", "id")
                 .addLike(nipam, "pegawai", "nipam")
                 .addLike(nama, "pegawai", "biodata", "nama")
-                .addEqual(tahun, "tahun")
+                .addCustom((root, cb) -> createTahunPredicate(root, cb, tahun))
                 .addEqual(jabatanId, "jabatan", "id")
                 .addEqual(picSaatIniId, "picSaatIni", "id")
                 .addEqual(approvalCutiStatus, "approvalCutiStatus")
                 .addEqual(jenisPengajuanCuti, "jenisPengajuanCuti")
                 .build();
+    }
+
+    private Predicate createTahunPredicate(Root<CutiPegawai> root, CriteriaBuilder cb, Integer tahun) {
+        if (tahun == null) return null;
+
+        Path<LocalDate> createdAtExpression = root.get("createdAt");
+        Path<LocalDate> tanggalMulaiExpression = root.get("tanggalMulai");
+        return cb.or(
+                cb.equal(cb.function("YEAR", Integer.class, createdAtExpression), tahun),
+                cb.equal(cb.function("MONTH", Integer.class, tanggalMulaiExpression), tahun)
+        );
     }
 }
